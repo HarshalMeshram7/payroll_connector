b67eacfe6152e6401e586c500304972cd3162538
root <root@hrms.dm40rbz4fv1u5lm0yayz1tjode.bx.internal.cloudapp.net>
2023-12-02 06:00:21 +0000

    sql script added for menu adjustment

---
+++ payroll.sql
@@ -0,0 +1,20 @@
+UPDATE ohrm_menu_item SET order_hint=400 where id=41;
+UPDATE ohrm_menu_item SET order_hint=300 where id=52;
+
+UPDATE ohrm_menu_item SET order_hint=99 where id=82;
+
+
+UPDATE ohrm_menu_item SET order_hint=500 where id=1;
+UPDATE ohrm_menu_item SET order_hint=100 where id=65;
+
+UPDATE ohrm_menu_item SET order_hint=1300 where id=1;
+UPDATE ohrm_menu_item SET order_hint=500 where id=105;
+
+UPDATE ohrm_menu_item SET order_hint=2100 where id=40;
+UPDATE ohrm_menu_item SET order_hint=600 where id=113;
+
+
+
+
+
+
